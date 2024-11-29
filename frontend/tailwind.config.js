ef3041b216fdc738bfde45bb453fa16376550511
devart002 <matt.contact1025@gmail.com>
2024-11-29 09:46:22 -0500

    feat: define h tag styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        h1: ["2.25rem", { lineHeight: "2.5rem" }], // 36px
+        h2: ["1.875rem", { lineHeight: "2.25rem" }], // 30px
+        h3: ["1.5rem", { lineHeight: "2rem" }], // 24px
+        h4: ["1.25rem", { lineHeight: "1.75rem" }], // 20px
+        h5: ["1.125rem", { lineHeight: "1.5rem" }], // 18px
+        h6: ["1rem", { lineHeight: "1.5rem" }], // 16px
+      },
       fontFamily: {
         verdana: ["Verdana", "sans-serif"],
       },
