b1ba41f06f286f6f81786d54e738566e5e5fc10f
devart002 <matt.contact1025@gmail.com>
2024-12-02 08:12:49 -0500

    chore: fix bug of type import in outdated component

--- frontend/app/components/ActivityLog.tsx
+++ frontend/app/components/ActivityLog.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import { ActivityLog } from "../types";
+import { IActivityLog } from "../types";
 
 const useTypewriter = (text: string, speed: number, chunkSize: number) => {
   const [displayedText, setDisplayedText] = useState("");
@@ -23,19 +23,19 @@ const useTypewriter = (text: string, speed: number, chunkSize: number) => {
 
 const ActivityLogList = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
-  const [agentLogs, setAgentLogs] = useState<ActivityLog[]>([]);
+  const [agentLogs, setAgentLogs] = useState<IActivityLog[]>([]);
 
   const lastLog = agentLogs.length > 0 ? agentLogs[agentLogs.length - 1] : null;
   const typewriterText = useTypewriter(
     lastLog ? lastLog.description : "",
     100,
-    20
+    20,
   );
 
   useEffect(() => {
     const fetchLogs = async () => {
       const response = await fetch(
-        "https://fnfscfgwilvky5z8.public.blob.vercel-storage.com/sami-logs.json"
+        "https://fnfscfgwilvky5z8.public.blob.vercel-storage.com/sami-logs.json",
       );
       const data = await response.json();
 
