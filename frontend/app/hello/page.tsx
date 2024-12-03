816310a24e9020438120e5a7490c4ca3e242ed79
devart002 <matt.contact1025@gmail.com>
2024-12-03 07:32:45 -0500

    fix: rid of 'The Future of Legacy Media'

--- frontend/app/hello/page.tsx
+++ frontend/app/hello/page.tsx
@@ -4,9 +4,6 @@ export default function Hello() {
   return (
     <main className="flex flex-col p-6 pt-10 pb-16 h-full items-center">
       <h1 className="text-6xl font-normal text-center mb-4">SamiOne</h1>
-      <h6 className="font-normal text-center mb-10">
-        The Future of Legacy Media
-      </h6>
       <Card
         className="h-0 flex-grow max-w-[1024px]"
         title="hello world!"
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main className="flex flex-col p-6 pt-10 pb-16 h-full items-center">
       <h1 className="text-6xl font-normal text-center mb-4">SamiOne</h1>
-      <h6 className="font-normal text-center mb-10">
-        The Future of Legacy Media
-      </h6>
       <ClientPage />
     </main>
   );
