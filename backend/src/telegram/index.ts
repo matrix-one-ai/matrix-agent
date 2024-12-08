31667a6afcdc18929160897b61859cd29b5e4367
Christopher Trimboli <cjft@trimboli.io>
2024-12-08 02:48:21 -0700

    TG agent contract analysis

--- backend/src/telegram/index.ts
+++ backend/src/telegram/index.ts
@@ -1,6 +1,11 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
-import { telegramReplyPrompt } from "./prompt";
+import {
+  TelegramAction,
+  telegramCryptoAnalysis,
+  telegramJudgementPrompt,
+  telegramReplyPrompt,
+} from "./prompt";
 import sami from "../characters/sami";
 import { generateTextFromPrompt } from "../ai";
 
@@ -12,26 +17,130 @@ export const telegramAgentInit = () => {
   bot.launch();
 
   bot.on(message("text"), async (ctx) => {
-    const prompt = telegramReplyPrompt(
-      sami,
-      ctx.message.text,
-      ctx.message.from.username!,
-      previousMessages.join("\n")
-    );
+    try {
+      const judgement = await generateTextFromPrompt(
+        telegramJudgementPrompt(ctx.message.text),
+        "gpt-4o",
+        {
+          temperature: 0.3,
+          frequencyPenalty: 0.3,
+          presencePenalty: 0.3,
+        }
+      );
 
-    const reply = await generateTextFromPrompt(prompt, "gpt-4o-mini", {
-      temperature: 0.5,
-      frequencyPenalty: 0.5,
-      presencePenalty: 0.5,
-    });
+      if (!judgement?.text) {
+        console.log("No judgement generated.");
+        return;
+      }
 
-    if (!reply?.text) {
-      console.log("No reply generated.");
-      return;
+      const judgementJson = JSON.parse(judgement.text);
+
+      console.log(judgementJson);
+
+      if (judgementJson.type === TelegramAction.contractAnalysis) {
+        console.log("Contract analysis requested.");
+
+        const platforms = [
+          "ethereum",
+          "polkadot",
+          "flow",
+          "avalanche",
+          "optimistic-ethereum",
+          "stellar",
+          "near-protocol",
+          "hedera-hashgraph",
+          "zksync",
+          "tron",
+          "celo",
+          "arbitrum-one",
+          "base",
+          "polygon-pos",
+          "solana",
+        ];
+
+        let tokenInfoResp = null;
+
+        for (const platform of platforms) {
+          const resp = await fetch(
+            `https://pro-api.coingecko.com/api/v3/coins/${platform}/contract/${judgementJson.contract}`,
+            {
+              headers: {
+                "x-cg-pro-api-key": process.env.COINGECKO_API_KEY!,
+              },
+            }
+          );
+
+          if (resp.ok) {
+            tokenInfoResp = resp;
+            break;
+          } else {
+            continue;
+          }
+        }
+
+        if (!tokenInfoResp) {
+          return await ctx.telegram.sendMessage(
+            ctx.message.chat.id,
+            "I'm sorry, I don't have realtime data on that contract."
+          );
+        }
+
+        const tokenInfo = await tokenInfoResp.json();
+
+        const reply = await generateTextFromPrompt(
+          telegramCryptoAnalysis(
+            sami,
+            tokenInfo,
+            ctx.message.text,
+            ctx.message.from.username!
+          ),
+          "gpt-4o-mini",
+          {
+            temperature: 0.3,
+            frequencyPenalty: 0.3,
+            presencePenalty: 0.3,
+          }
+        );
+
+        if (!reply?.text) {
+          console.log("No reply generated.");
+          return;
+        }
+
+        previousMessages.push(ctx.message.text);
+
+        return await ctx.telegram.sendMessage(ctx.message.chat.id, reply.text);
+      }
+
+      if (judgementJson.type === TelegramAction.simpleReply) {
+        console.log("Simple reply requested.");
+
+        const reply = await generateTextFromPrompt(
+          telegramReplyPrompt(
+            sami,
+            ctx.message.text,
+            ctx.message.from.username!,
+            previousMessages.join("\n")
+          ),
+          "gpt-4o-mini",
+          {
+            temperature: 0.5,
+            frequencyPenalty: 0.5,
+            presencePenalty: 0.5,
+          }
+        );
+
+        if (!reply?.text) {
+          console.log("No reply generated.");
+          return;
+        }
+
+        previousMessages.push(ctx.message.text);
+
+        return await ctx.telegram.sendMessage(ctx.message.chat.id, reply.text);
+      }
+    } catch (e) {
+      console.log(e);
     }
-
-    previousMessages.push(ctx.message.text);
-
-    await ctx.telegram.sendMessage(ctx.message.chat.id, reply.text);
   });
 };
--- backend/src/telegram/prompt.ts
+++ backend/src/telegram/prompt.ts
@@ -1,5 +1,45 @@
 import { Character } from "../characters";
 
+export enum TelegramAction {
+  simpleReply = "simpleReply",
+  contractAnalysis = "contractAnalysis",
+}
+
+export const telegramJudgementPrompt = (message: string) => {
+  return `
+Judge the message and decide the function action to take.
+Message: ${message}
+
+Options:
+- simpleReply
+- contractAnalysis
+
+Output only 1 of the action options.
+
+Example:
+
+Message: Hey, how are you doing?
+
+Output: { type: "simpleReply" }
+
+--- OR ---
+
+Message: What is 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 price?
+
+Output: 
+
+{ "type": "contractAnalysis", "contract": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48" }
+
+Message: What is 1337 price?
+
+Output: 
+
+{ "type": "contractAnalysis", "contract": "1337" }
+
+Only output the JSON.
+`;
+};
+
 export const telegramReplyPrompt = (
   character: Character,
   message: string,
@@ -7,26 +47,61 @@ export const telegramReplyPrompt = (
   previousChannelMessages: string
 ) => {
   return `
-  You have recived a telegram message to reply to in a conversation.
-  Generate a unique discord channel reply in the voice and style of ${character.name}.
-  - Name ${character.name}
+You have recived a telegram message to reply to in a conversation.
+Generate a unique discord channel reply in the voice and style of ${character.name}.
+- Name ${character.name}
+- Age: ${character.age}
+- Bio: ${character.bio}
+- Occupation: ${character.occupation}
+- Appearance: ${character.appearance}
+- Personality: ${character.personality}
+
+Do not add commentary or acknowledge this request, just write the reply.
+Brief, concise statements only. No not use emojis.
+Feel more unique / personal.
+If the user asks for code, you can output code inside \`\`\` \`\`\` triple brackets.
+
+Message to reply to:
+${message}
+
+Username to reply to: ${username}
+
+Previous channel conversation messages for context:
+${previousChannelMessages}
+`;
+};
+
+export const telegramCryptoAnalysis = (
+  character: Character,
+  tokenInfo: any,
+  userMessage: string,
+  username: string
+) => {
+  return `
+  Analyze the trending token. Provide a response to the trending token.
+  Act like a crypto investor expert. Make it informational.
+  Generate a discord reply in the voice and style of ${character.name}.
   - Age: ${character.age}
   - Bio: ${character.bio}
   - Occupation: ${character.occupation}
   - Appearance: ${character.appearance}
   - Personality: ${character.personality}
   
-  Do not add commentary or acknowledge this request, just write the reply.
-  Brief, concise statements only. No not use emojis.
-  Feel more unique / personal.
-  If the user asks for code, you can output code inside \`\`\` \`\`\` triple brackets.
+  Token Name: ${tokenInfo.name}
+  Token Symbol: ${tokenInfo.symbol}
   
-  Message to reply to:
-  ${message}
+  Token JSON info dump:
+  
+  ${JSON.stringify(tokenInfo, null, 2)}
+  
+  Do not add commentary or acknowledge this request, just write the reply.
+  Your response should not contain any questions. Brief, concise statements only. No emojis.
+  Do not mention emails or phone numbers. No hashtags. Use $TOKEN when saying token names.
+  Do not intro or outro the response, just the response.
+  
+  User message to reply to:
+  ${userMessage}
   
   Username to reply to: ${username}
-  
-  Previous channel conversation messages for context:
-  ${previousChannelMessages}
     `;
 };
