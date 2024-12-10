f4e7068bff7e786978a938c251a57affb7841c4d
Christopher Trimboli <cjft@trimboli.io>
2024-12-10 09:36:08 -0700

    setup resend email template and test API

---
+++ frontend/app/components/EmailTemplate.tsx
@@ -0,0 +1,17 @@
+interface EmailTemplateProps {
+  giftMessage: string;
+  amount: string;
+  currency: string;
+  redeemCode: string;
+}
+
+const EmailTemplate = ({
+  giftMessage,
+  amount,
+  currency,
+  redeemCode,
+}: EmailTemplateProps) => {
+  return <div></div>;
+};
+
+export default EmailTemplate;
