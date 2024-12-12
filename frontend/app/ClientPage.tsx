1d04081ae3a7267feb638bb34ba3c78a540ee654
devart002 <matt.contact1025@gmail.com>
2024-12-12 13:49:55 -0500

    chore: adjust placeholder cards

--- frontend/app/ClientPage.tsx
+++ frontend/app/ClientPage.tsx
@@ -466,15 +466,6 @@ const ClientPage = () => {
           <Card title="<placeholder>" level={1} maxLevel={5}>
             <Image
               className="w-full"
-              src="/images/placeholder_12.png"
-              alt=""
-              width={256}
-              height={256}
-            />
-          </Card>
-          <Card title="<placeholder>" level={1} maxLevel={5}>
-            <Image
-              className="w-full"
               src="/images/placeholder_11.png"
               alt=""
               width={256}
