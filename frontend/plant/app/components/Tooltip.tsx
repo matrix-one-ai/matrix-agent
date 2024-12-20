854c84a5807035623b91130811c26d78c03fa7cc
devart002 <matt.contact1025@gmail.com>
2024-12-20 09:56:43 -0500

    fix: rid of tooltip arrow tails

--- frontend/plant/app/components/Tooltip.tsx
+++ frontend/plant/app/components/Tooltip.tsx
@@ -19,14 +19,15 @@ const Tooltip: React.FC<ITooltipProps> = ({
     <MTTooltip
       className={clsx(
         "bg-white text-black font-bold max-w-80 mx-2 p-1 rounded-lg drop-shadow-md text-center",
-        placement === "top" &&
-          "before:content-[''] before:absolute before:top-full before:left-1/2 before:-translate-x-1/2 before:border-8 before:border-transparent before:border-t-white",
-        placement === "bottom" &&
-          "before:content-[''] before:absolute before:bottom-full before:left-1/2 before:-translate-x-1/2 before:border-8 before:border-transparent before:border-b-white",
-        placement === "left" &&
-          "before:content-[''] before:absolute before:left-full before:top-1/2 before:-translate-y-1/2 before:border-8 before:border-transparent before:border-l-white",
-        placement === "right" &&
-          "before:content-[''] before:absolute before:right-full before:top-1/2 before:-translate-y-1/2 before:border-8 before:border-transparent before:border-r-white",
+        // TODO: Disabling arrow tail for now
+        // placement === "top" &&
+        //   "before:content-[''] before:absolute before:top-full before:left-1/2 before:-translate-x-1/2 before:border-8 before:border-transparent before:border-t-white",
+        // placement === "bottom" &&
+        //   "before:content-[''] before:absolute before:bottom-full before:left-1/2 before:-translate-x-1/2 before:border-8 before:border-transparent before:border-b-white",
+        // placement === "left" &&
+        //   "before:content-[''] before:absolute before:left-full before:top-1/2 before:-translate-y-1/2 before:border-8 before:border-transparent before:border-l-white",
+        // placement === "right" &&
+        //   "before:content-[''] before:absolute before:right-full before:top-1/2 before:-translate-y-1/2 before:border-8 before:border-transparent before:border-r-white",
       )}
       placement={placement}
       content={content}
