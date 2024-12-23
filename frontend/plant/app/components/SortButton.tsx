8bc7462b60cf4a0a80a0993e7f8b5ac37e6f48ca
devart002 <matt.contact1025@gmail.com>
2024-12-23 07:57:07 -0500

    fix: fix on sort button

--- frontend/plant/app/components/SortButton.tsx
+++ frontend/plant/app/components/SortButton.tsx
@@ -17,22 +17,18 @@ const SortButton: React.FC<ISortButtonProps> = ({
       onClick={onClick}
       {...rest}
     >
-      <div className="relative w-0 h-0 border-l-[5px] border-r-[5px] border-b-[6px] border-l-transparent border-r-transparent border-b-black">
-        <span
-          className={clsx(
-            "absolute top-[1.5px] -left-[3px] w-0 h-0 border-l-[3px] border-r-[3px] border-b-[3.5px] border-l-transparent border-r-transparent border-b-secondary",
-            direction === "desc" && "border-b-[#989898]",
-          )}
-        />
-      </div>
-      <div className="relative w-0 h-0 border-l-[5px] border-r-[5px] border-t-[6px] border-l-transparent border-r-transparent border-t-black">
-        <span
-          className={clsx(
-            "absolute -top-[5px] -left-[3px] w-0 h-0 border-l-[3px] border-r-[3px] border-t-[3.5px] border-l-transparent border-r-transparent border-t-secondary",
-            direction === "asc" && "border-t-[#989898]",
-          )}
-        />
-      </div>
+      <div
+        className={clsx(
+          "relative w-0 h-0 border-l-[5px] border-r-[5px] border-b-[6px] border-l-transparent border-r-transparent",
+          direction === "desc" ? "border-b-[#989898]" : "border-b-secondary",
+        )}
+      />
+      <div
+        className={clsx(
+          "relative w-0 h-0 border-l-[5px] border-r-[5px] border-t-[6px] border-l-transparent border-r-transparent",
+          direction === "asc" ? "border-t-[#989898]" : "border-t-secondary",
+        )}
+      />
     </button>
   );
 };
