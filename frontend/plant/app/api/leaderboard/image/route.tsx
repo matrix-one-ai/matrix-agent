b974352efa043247bd58fa020448b47477ad1594
devart002 <matt.contact1025@gmail.com>
2025-01-06 17:32:48 -0500

    feat: og image generation for new user joining

--- frontend/plant/app/api/leaderboard/image/route.tsx
+++ frontend/plant/app/api/leaderboard/image/route.tsx
@@ -1,13 +1,15 @@
 import { ImageResponse } from "@vercel/og";
 import { NextRequest, NextResponse } from "next/server";
 
-// matt plz finish / fix this API.
+const loadFont = async (fontUrl: string): Promise<ArrayBuffer> => {
+  const response = await fetch(fontUrl);
+  if (!response.ok) {
+    throw new Error("Font failed to load");
+  }
+  return response.arrayBuffer();
+};
 
-// 1.) Ian needs to orchasterate when based on rules of leaderboard, a user is picked for a Twitter post. (don't spam, no duplicates, user can only be featured once).
-// 2.) Ian fetches: https://plant.fun/api/leaderboard/image?userName=cjft&twitterAvatarUrl=https://pbs.twimg.com/profile_images/1440000000000000000/aaaaaaaa_normal.jpg GET -> return .png
-// 3.) Ian used .png returned from request, posts a tweet with v2 API like: "Welcome @cjft to the leaderboard! 🌵🎉 <attach image>"
-
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const userName = searchParams.get("userName");
   const twitterAvatarUrl = searchParams.get("twitterAvatarUrl");
@@ -20,29 +22,94 @@ export function GET(request: NextRequest) {
     return NextResponse.json("Missing twitterAvatarUrl", { status: 400 });
   }
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: "flex",
-          fontSize: 40,
-          color: "white",
-          background: "black",
-          width: "100%",
-          height: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        Hello {userName}!
-        <img
-          src={twitterAvatarUrl}
-          alt="User Avatar"
-          width={200}
-          height={200}
-        />
-      </div>
-    ),
-    { width: 1200, height: 630 }
-  );
+  try {
+    const urlOrigin =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}`
+          : "https://plant.fun";
+    const fontData = await loadFont(
+      `${urlOrigin}/fonts/Comic Sans MS Bold.ttf`,
+    );
+    const backgroundImage = `${urlOrigin}/images/post.png`;
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            position: "relative",
+            display: "flex",
+            width: "100%",
+            height: "100%",
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "1200px 630px",
+            backgroundPosition: "center",
+            boxSizing: "border-box",
+            padding: "20px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              width: "72%",
+              padding: "36px",
+              paddingTop: "96px",
+              gap: "24px",
+            }}
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={twitterAvatarUrl}
+              alt="User Avatar"
+              width={150}
+              height={150}
+              style={{
+                borderRadius: "50%",
+              }}
+            />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: 0,
+                flexGrow: 1,
+                gap: "18px",
+              }}
+            >
+              <span
+                style={{
+                  fontSize: 38,
+                  color: "#70C238",
+                }}
+              >
+                @{userName}
+              </span>
+              <span style={{ fontSize: 38, color: "black" }}>
+                I am a Gardener for Plant and will be showered with
+                Rain(air)drops!
+              </span>
+            </div>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: "Inter",
+            data: fontData,
+            style: "normal",
+            weight: 400,
+          },
+        ],
+      },
+    );
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return NextResponse.json(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
