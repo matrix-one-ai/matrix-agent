20a907e4bf17717125b9932041f3d6d78ca030c2
devart002 <matt.contact1025@gmail.com>
2025-01-20 11:14:21 -0500

    fix: update ctas on coming soon rising sprouts box again

--- frontend/plant/app/components/RisingSproutsComingSoon.tsx
+++ frontend/plant/app/components/RisingSproutsComingSoon.tsx
@@ -9,18 +9,14 @@ const RisingSproutsComingSoon = () => {
       title="Rising Sprouts"
     >
       <div className="relative w-full h-[50vh] flex-grow overflow-auto p-2 md:p-4 font-bold flex flex-col items-center justify-center">
-        <p className="text-center text-2xl mb-4">
-          🚧 Coming soon! 🚧
-          <br />
-          Exclusive Rewards for $PLANT Stakers
-        </p>
+        <p className="text-center text-2xl mb-4">🚧 Coming soon! 🚧</p>
         <p className="text-center text-sm mb-6">
-          Stake 500,000+ $PLANT to unlock Rising Sprouts
-          <br />
-          high-potential tokens analyzed by Plant as soon as they hit Raydium or
+          High-potential tokens analyzed by Plant as soon as they hit Raydium or
           Meteora.
         </p>
-        <p className="text-center text-base mb-2">Secure Your Early Access</p>
+        <p className="text-center text-base mb-2">
+          Stake 500,000+ $PLANT to unlock Rising Sprouts and secure early access
+        </p>
         <Link
           className="px-4 py-1 border border-black rounded-[100px] hover:bg-[#F9E9C4]"
           href="https://app.wasabi.xyz/vaults?vault=sPLANT&network=solana"
