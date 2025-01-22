16ba8d80a73058be3a74cadd2a8470ef98734aaa
devart002 <matt.contact1025@gmail.com>
2025-01-22 12:57:07 -0500

    chore: airdrop does on Friday

--- frontend/plant/app/constants/index.ts
+++ frontend/plant/app/constants/index.ts
@@ -4,7 +4,7 @@ export const LB_PLACEHOLDER_AVATAR_SRC = "/images/lb_placeholder_avatar.png";
 
 export const LB_PAGE_COUNT_LIMIT = 5;
 
-export const AIRDROP_START_DATE = new Date(2025, 0, 15);
+export const AIRDROP_START_DATE = new Date(2025, 0, 17);
 
 export const AIRDROP_INTERVAL = 7;
 
